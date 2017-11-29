31bf8750edb51c1cb6ad0be14955d10b611f95dc
Kim Skildheim <kimskildheim@gmail.com>
2017-11-29 13:29:47 +0100

    Added properties from both pure config and annotaitons through XML-config

--- src/main/java/no/skildheim/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/no/skildheim/repository/HibernateCustomerRepositoryImpl.java
@@ -1,14 +1,24 @@
 package no.skildheim.repository;
 
 import no.skildheim.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
+    // Example of injection properties without annotations
+    // public void setDbUsername(String dbUsername) {
+    //     this.dbUsername = dbUsername;
+    // }
+
     @Override
     public List<Customer> findAll(){
+        System.out.println(dbUsername);
         List<Customer> customers = new ArrayList<>();
         Customer customer = new Customer();
 
